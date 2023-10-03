25b4852e44d1742a21bdc48d744ca3a7a8556022
Yogesh Chandra Singh Samant <yogeshchsamant@gmail.com>
2023-10-03 18:32:49 +0530

    Calling Geocoding API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,35 @@
 import { ChangeEvent, useState } from 'react'
 
-const App = (): JSX.Element => {
-  const [term, setTerm] = useState<string>('')
+import { optionType } from './types'
 
-  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTerm(e.target.value)
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+const App = (): JSX.Element => {
+  const [term, setTerm] = useState<string>('') // term is the place name
+  const [options, setOptions] = useState<[]>([])
+
+  const getSearchOptions = (value: string) => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&appid=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => setOptions(data))
   }
 
-  // https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim()
+    setTerm(value)
+
+    if (value === '') return
+
+    getSearchOptions(value)
+  }
+
+  const onOptionSelect = (option: optionType) => {
+    console.log(option.name)
+  }
 
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
@@ -22,13 +44,27 @@ const App = (): JSX.Element => {
           Search and select the city from the dropdown
         </p>
 
-        <div className="flex mt-10 md:mt-4">
+        <div className="relative flex mt-10 md:mt-4">
           <input
             type="text"
             value={term}
             className="px-2 py-1 rounded-l-md border-2 border-white "
             onChange={onInputChange}
           />
+
+          <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
+            {options.map((option: optionType, index: number) => (
+              <li key={option.name + '-' + index}>
+                <button
+                  className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer"
+                  onClick={() => onOptionSelect(option)}
+                >
+                  {option.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+
           <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py cursor-pointer">
             Search
           </button>
