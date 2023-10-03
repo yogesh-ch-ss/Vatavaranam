5b2194435d480aa7f61d2563f31ac6eb75fe1958
Yogesh Chandra Singh Samant <yogeshchsamant@gmail.com>
2023-10-03 19:48:17 +0530

    Calling Weather Forecast API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useState, useEffect } from 'react'
 
 import { optionType } from './types'
 
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
-// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
 const App = (): JSX.Element => {
-  const [term, setTerm] = useState<string>('') // term is the place name
-  const [options, setOptions] = useState<[]>([])
+  const [term, setTerm] = useState<string>('') // term is whatever is entered on the input field
+  const [options, setOptions] = useState<[]>([]) // options is the data received from the geolocation api - options of cities visible on search options
+  const [city, setCity] = useState<optionType | null>(null) // city is set after selecting a city from the search options
 
   const getSearchOptions = (value: string) => {
     fetch(
@@ -27,9 +28,33 @@ const App = (): JSX.Element => {
     getSearchOptions(value)
   }
 
+  // Acts after selecting an "option" of cities from the list of "options"
   const onOptionSelect = (option: optionType) => {
-    console.log(option.name)
+    setCity(option)
   }
+
+  // Gets the forecast data of the entered city
+  const getForecast = (city: optionType) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&appid=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log({ data }))
+  }
+
+  // Acts after the Submit button is clicked
+  const onSubmit = () => {
+    if (!city) return
+
+    getForecast(city)
+  }
+
+  useEffect(() => {
+    if (city) {
+      setTerm(city.name)
+      setOptions([])
+    }
+  }, [city])
 
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
@@ -65,7 +90,7 @@ const App = (): JSX.Element => {
             ))}
           </ul>
 
-          <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py cursor-pointer">
+          <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py cursor-pointer" onClick={onSubmit}>
             Search
           </button>
         </div>
