7c564fa4542b3d82309779aaa0eda7716d5d47e0
Yogesh Chandra Singh Samant <yogeshchsamant@gmail.com>
2023-10-04 20:56:38 +0530

    Chage in theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,9 +7,10 @@ const App = (): JSX.Element => {
     useForecast()
 
   return (
-    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+    // <main className="flex justify-center items-center bg-gradient-to-b from-orange-300 to-rose-300 h-[100vh] w-full">
+    <main className="flex justify-center items-center bg-gradient-to-br from-pink-300 via-purple-400 to-indigo-400 h-[100vh] w-full">
       {forecast ? (
-        <Forecast data={forecast}/>
+        <Forecast data={forecast} />
       ) : (
         <Search
           term={term}
--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -18,9 +18,9 @@ const Search = ({
   onSubmit,
 }: Props): JSX.Element => {
   return (
-    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+    <main className="flex justify-center items-center h-[100vh] w-full">
       <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10-lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700">
-        <h1 className="text-4xl font-semibold font-mono px-4 py-4">
+        <h1 className="text-4xl font-semibold font-mono px-4 py-4 bg-gradient-to-r from-slate-700 via-purple-800 to-slate-700 bg-clip-text text-transparent">
           vatavaranam
         </h1>
         <h2 className="text-2xl font-light">
@@ -34,7 +34,7 @@ const Search = ({
           <input
             type="text"
             value={term}
-            className="px-2 py-1 rounded-l-md border-2 border-white "
+            className="px-2 py-1 rounded-l-md border-2 border-white"
             onChange={onInputChange}
           />
 
@@ -52,7 +52,10 @@ const Search = ({
           </ul>
 
           <button
-            className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py cursor-pointer"
+            className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-600 hover:text-zinc-600
+
+            
+            text-zinc-100 px-2 py cursor-pointer"
             onClick={onSubmit}
           >
             Search
