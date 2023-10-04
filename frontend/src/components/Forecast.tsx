75030dd43ba0489db3aa1b44d01043a57e0daf96
Yogesh Chandra Singh Samant <yogeshchsamant@gmail.com>
2023-10-04 19:07:31 +0530

    Tiles of other data

--- frontend/src/components/Forecast.tsx
+++ frontend/src/components/Forecast.tsx
@@ -1,4 +1,14 @@
+import {
+  getHumidityValue,
+  getPop,
+  getSunTime,
+  getVisibilityValue,
+  getWindDirection,
+} from '../helpers'
 import { forecastType } from '../types'
+import Sunrise from './Icons/Sunrise'
+import Sunset from './Icons/Sunset'
+import Tile from './Tile'
 
 type Props = {
   data: forecastType
@@ -59,7 +69,72 @@ const Forecast = ({ data }: Props): JSX.Element => {
         </section>
 
         {/* Tiles */}
-        <section>
+        <section className="flex flex-wrap justify-between text-zinc-700">
+          {/* sunrise */}
+          <div className="w-[140px] text-xs font-bold flex flex-col items-center bg-white/20 backdrop-blur-lg rounded drop-shadow-lg py-4 mb-5">
+            <Sunrise /> <span className="mt-2">{getSunTime(data.sunrise)}</span>
+          </div>
+
+          {/* sunset */}
+          <div className="w-[140px] text-xs font-bold flex flex-col items-center bg-white/20 backdrop-blur-lg rounded drop-shadow-lg py-4 mb-5">
+            <Sunset /> <span className="mt-2">{getSunTime(data.sunset)}</span>
+          </div>
+
+          {/* wind */}
+          <Tile
+            icon="wind"
+            title="Wind"
+            info={`${Math.round(today.wind.speed)} km/h`}
+            description={`${getWindDirection(
+              Math.round(today.wind.deg)
+            )}, gust ${today.wind.gust.toFixed(1)} km/h`}
+          />
+
+          {/* feels like */}
+          <Tile
+            icon="feels"
+            title="Feels like"
+            info={<Degree temp={Math.round(today.main.feels_like)} />}
+            description={`Feels ${
+              Math.round(today.main.feels_like) > Math.round(today.main.temp)
+                ? 'warmer'
+                : 'colder'
+            }`}
+          />
+
+          {/* humidity */}
+          <Tile
+            icon="humidity"
+            title="Humidity"
+            info={`${today.main.humidity}%`}
+            description={`${getHumidityValue(today.main.humidity)}`}
+          />
+
+          {/* precipitation / pop */}
+          <Tile
+            icon="pop"
+            title="Precipitation"
+            info={`${Math.round(today.pop * 100)}%`}
+            description={`${getPop(today.pop)}, clouds at ${today.clouds.all}%`}
+          />
+
+          {/* pressure */}
+          <Tile
+            icon="pressure"
+            title="Pressure"
+            info={`${Math.round(today.main.pressure)} hPa`}
+            description={`${
+              Math.round(today.main.pressure) < 1013 ? 'Lower' : 'Higher'
+            } than standard`}
+          />
+
+          {/* visibility */}
+          <Tile
+            icon="visibility"
+            title="Visibility"
+            info={`${(today.visibility / 1000).toFixed()} kms`}
+            description={getVisibilityValue(today.visibility)}
+          />
 
         </section>
       </div>
---
+++ frontend/src/components/Tile.tsx
@@ -0,0 +1,38 @@
+import Feels from './Icons/Feels'
+import Pop from './Icons/Pop'
+import Pressure from './Icons/Pressure'
+import Visibility from './Icons/Visibility'
+import Wind from './Icons/Wind'
+import Humidity from './Icons/Wind'
+
+type Props = {
+  icon: 'wind' | 'feels' | 'humidity' | 'pop' | 'pressure' | 'visibility'
+  title: string
+  info: string | JSX.Element
+  description: string
+}
+
+const icons = {
+  wind: Wind,
+  feels: Feels,
+  humidity: Humidity,
+  pop: Pop,
+  pressure: Pressure,
+  visibility: Visibility,
+}
+
+const Tile = ({ icon, title, info, description }: Props): JSX.Element => {
+  const Icon = icons[icon]
+
+  return (
+    <article className="w-[140px] h-[130px] text-zinc-700 bg-white/20 backdrop-blur-lg rounded drop-shadow-lg p-2 mb-5 flex flex-col justify-between">
+      <div className="flex items-center text-sm font-bold">
+        <Icon /> <h4 className="ml-1">{title}</h4>
+      </div>
+      <h3 className="mt-2 text-lg">{info}</h3>
+      <p className="text-xs font-bold">{description}</p>
+    </article>
+  )
+}
+
+export default Tile
