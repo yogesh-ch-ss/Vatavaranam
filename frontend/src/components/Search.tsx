99aba137292352d18f0e86319e060ef83adbfc3a
Yogesh Chandra Singh Samant <yogeshchsamant@gmail.com>
2023-10-05 19:53:28 +0530

    Search page description

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -20,15 +20,18 @@ const Search = ({
   return (
     <main className="flex justify-center items-center h-[100vh] w-full">
       <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10-lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700">
-        <h1 className="text-4xl font-semibold font-mono px-4 py-4 bg-gradient-to-r from-slate-700 via-purple-800 to-slate-600 bg-clip-text text-transparent">
+        <h1 className="text-4xl font-semibold font-mono px-4 py-4 bg-gradient-to-r from-slate-600 via-purple-800 to-slate-600 bg-clip-text text-transparent">
           vatavaranam
         </h1>
         <h2 className="text-2xl font-light">
           Weather <span className="font-black">Forecast</span>
         </h2>
-        <p className="text-sm mt-2">
-          Search and select the city from the dropdown
+        <p className="mt-2">
+          <span className="font-semibold">Search</span> and{' '}
+          <span className="font-semibold">select</span> the city or area from the
+          dropdown.
         </p>
+        <p className="text-xs mt-2 font-medium text-">Use <span className="font-mono">"_"</span> for space. Ex: San_Francisco</p>
 
         <div className="relative flex mt-10 md:mt-4">
           <input
